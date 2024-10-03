70411c2c7ed39fd9b5969c83c71da30dc70d1516
ahmadsaflo1 <ahmadsaflo.kth@gmail.com>
2024-10-03 18:00:26 +0200

    renamed

--- src/main/java/se/kth/ahmad_nedal/distributed_systems_java/BO/Product.java
+++ src/main/java/se/kth/ahmad_nedal/distributed_systems_java/BO/Product.java
@@ -1,11 +1,11 @@
 package se.kth.ahmad_nedal.distributed_systems_java.BO;
 
-public class product {
+public class Product {
     private int id;
     private String name;
     private double price;
 
-    public product(int id, String name, double price) {
+    public Product(int id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
