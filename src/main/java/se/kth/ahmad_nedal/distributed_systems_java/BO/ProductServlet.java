ac5bf3f2f03ee2d9e14c51eb90e4c1f90e4b8c89
nedal <nedalh@kth.se>
2024-10-04 16:11:13 +0200

    fixa servlet som hämtar listan från databasen och skickar listan till produkt

---
+++ src/main/java/se/kth/ahmad_nedal/distributed_systems_java/BO/ProductServlet.java
@@ -0,0 +1,24 @@
+package se.kth.ahmad_nedal.distributed_systems_java.BO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import se.kth.ahmad_nedal.distributed_systems_java.DB.ProductDAO;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/products")
+public class ProductServlet extends HttpServlet {
+    private ProductDAO productDAO = new ProductDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Product> productList = productDAO.getProducts();
+        request.setAttribute("products", productList);
+        request.getRequestDispatcher("products.jsp").forward(request, response);
+    }
+}
